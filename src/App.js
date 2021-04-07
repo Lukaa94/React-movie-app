1588cb4e544d17a200bdb024ba028183633ef61e
Luka Juric <lukajuric98765@gmail.com>
2021-04-07 15:21:34 +0200

    added components file, nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Movies from "./Movies";
+import Nav from "./components/Nav";
+import Movies from "./components/Movies";
 import "./App.css";
 import urlList from "./services/urlList";
 
 function App() {
   return (
     <div>
+      <Nav />
       <Movies title="popular" url={urlList.popular} />
       <Movies title="top rated" url={urlList.topRated} />
       <Movies title="2020" url={urlList.year2020}/>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import baseApi from "./services/baseApi";
+import baseApi from "../services/baseApi";
+import "../components/Movie.css";
 
 const imgUrl = "https://image.tmdb.org/t/p/original";
 
