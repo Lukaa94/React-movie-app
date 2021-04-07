abf410a24312253de392b5d7781879770f339ab4
Luka Juric <lukajuric98765@gmail.com>
2021-04-07 18:29:04 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "../images/tmdb-logo.svg";
+import account_logo from "../images/account-logo.svg";
 import "../components/Header.css";
+
 
 function Header() {
   return (
@@ -12,13 +14,16 @@ function Header() {
       <div className="navbar-links">
         <ul>
           <li><a href="#">Home</a></li>
-          <li><a href="#">Tv shows</a></li>
+          <li><a href="#">TV Shows</a></li>
           <li><a href="#">Movies</a></li>
           <li><a href="#">New &amp; popular</a></li>
           <li><a href="#">My List</a></li>
         </ul>
       </div>
       </nav>
+      <div>
+        <img className="account-logo" src={account_logo} alt="account" />
+      </div>
     </header>
   );
 }
