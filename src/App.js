ffa8a385fd002b6f8eef12d615753e4bc0a820e5
Luka Juric <lukajuric98765@gmail.com>
2021-04-08 14:49:06 +0200

    overflow fix

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Nav from "./components/Header";
 import Movies from "./components/Movies";
 import "./App.css";
 import urlList from "./services/urlList";
+import Showcase from "./components/Showcase";
 
 function App() {
   return (
     <div>
       <Nav />
+      <Showcase />
       <Movies title="popular" url={urlList.popular} />
       <Movies title="top rated" url={urlList.topRated} />
       <Movies title="2020" url={urlList.year2020}/>
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import "../components/Showcase.css";
 
 function Showcase() {
     return (
-        <div>
+        <div className="Showcase">
             
         </div>
     )
