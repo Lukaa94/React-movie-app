af9f5c2d7a4a05759e2ac3acfd08a3631706d2fc
Luka Juric <lukajuric98765@gmail.com>
2021-04-09 12:59:50 +0200

    showcase text fix

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -27,6 +27,10 @@ function Showcase() {
         <div className="showcase-text">
             <h1>{movies.title}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis a debitis nam repudiandae odio suscipit velit ipsa. Exercitationem id, commodi tenetur quasi ut velit veniam ratione modi vero dolores nulla?</p>
+            <div className="showcase-btns">
+              <button className="btn-play">Play</button>
+              <button className="btn-info">More Info</button>
+            </div>
         </div>
       <img
         src={`${imgUrl}${movies.backdrop_path}`}
