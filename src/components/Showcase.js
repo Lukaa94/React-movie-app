41c2c7422b374ef1a55fb56b33bc1d870486054a
Luka Juric <lukajuric98765@gmail.com>
2021-04-09 14:09:33 +0200

    limit paragraph words

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -26,7 +26,7 @@ function Showcase() {
     <div className="Showcase">
         <div className="showcase-text">
             <h1>{movies.title}</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis a debitis nam repudiandae odio suscipit velit ipsa. Exercitationem id, commodi tenetur quasi ut velit veniam ratione modi vero dolores nulla?</p>
+            <p className="limit-words">{movies.overview}</p>
             <div className="showcase-btns">
               <button className="btn-play">Play</button>
               <button className="btn-info">More Info</button>
