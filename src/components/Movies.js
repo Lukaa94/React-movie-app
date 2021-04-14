e398a8606a5df076fdc2300868c647c063b955d4
Luka Juric <lukajuric98765@gmail.com>
2021-04-14 16:21:34 +0200

    inplement swiper

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination } from "swiper";
+import "swiper/swiper-bundle.css";
 import baseApi from "../services/baseApi";
 import "../components/Movies.css";
 
+//fix swiper on resize
+
 const imgUrl = "https://image.tmdb.org/t/p/original";
+
+SwiperCore.use([Navigation, Pagination ]);
 
 function Movies({ title, url }) {
   const [movies, setMovies] = useState([]);
-  
+
   useEffect(() => {
     async function fetchData() {
       const Api = await baseApi.get(url);
@@ -19,15 +26,42 @@ function Movies({ title, url }) {
   return (
     <div className="Movie">
       <h1 className="movie-title">{title}</h1>
-      <div className="movie-images">
+
+      <Swiper
+        className="swiper-container"
+        speed={700}
+        slidesPerView={2}
+        slidesPerGroup={2}
+        navigation
+        pagination
+        spaceBetween={20}
+        observer={true}
+        observeParents={true}
+        breakpoints={{
+          1024: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          900: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          768: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+      >
         {movies.map((movie) => (
-          <img
-            key={movie.id}
-            src={`${imgUrl}${movie.poster_path}`}
-            alt={movie.title}
-          />
+          <SwiperSlide className="swiper-slide">
+            <img
+              key={movie.id}
+              src={`${imgUrl}${movie.poster_path}`}
+              alt="img"
+            />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 }
